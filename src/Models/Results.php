37b67bfdbcc3966b094270050041eb8ab16db9eb
Björn Brala <bjorn@swis.nl>
2017-06-05 12:56:54 +0200

    Massa assignable attributes

--- src/Models/Results.php
+++ src/Models/Results.php
@@ -11,14 +11,38 @@ class Results extends Model
 {
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = array(
+      'remote',
+      'filename',
+      'line',
+      'author',
+      'author_normalized',
+      'author_slug',
+      'email',
+      'date',
+      'commitHash',
+      'parser',
+      'created_at',
+      'updated_at',
+      'file',
+      'parser',
+    );
+
+    /**
      * Create or update a record matching the attributes, and fill it with values.
      *
      * @param  array $attributes
      * @param  array $values
      * @return static
      */
-    public static function updateOrCreate(array $attributes, array $values = array())
-    {
+    public static function updateOrCreate(
+      array $attributes,
+      array $values = array()
+    ) {
         $instance = static::firstOrNew($attributes);
 
         $instance->fill($values)->save();
@@ -30,16 +54,20 @@ class Results extends Model
     {
         if (strpos($this->remote, 'bitbucket.org') > 0) {
             return str_replace(
-                '.git',
-                '',
-                str_replace('git@', 'https://', str_replace(':', '/', $this->remote))
-            ) . '/commits/' . $this->commitHash . '#L' . $this->filename . 'T' . $this->line;
-        } elseif(strpos($this->remote, 'github.com') > 0) {
+              '.git',
+              '',
+              str_replace(
+                'git@',
+                'https://',
+                str_replace(':', '/', $this->remote)
+              )
+            ).'/commits/'.$this->commitHash.'#L'.$this->filename.'T'.$this->line;
+        } elseif (strpos($this->remote, 'github.com') > 0) {
             return str_replace(
-                '.git',
-                '',
-                str_replace('git@', 'https://', $this->remote)
-            ) . '/blame/master/' . $this->filename . '#L' . $this->line;
+              '.git',
+              '',
+              str_replace('git@', 'https://', $this->remote)
+            ).'/blame/master/'.$this->filename.'#L'.$this->line;
         } else {
             return '#';
         }
